7cd7a2299e0224e002f76d2527cb9212683fcb0d
Denis Fernando <93955038+DenFerOli@users.noreply.github.com>
2026-05-25 22:36:14 -0300

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Orange HRM Tests', () => {
+  it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    cy.get('[name="username"]').type('Admin')
+    cy.get('[name="password"]').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+  })
+
+  it('Login - Failed', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+      cy.get('[name="username"]').type('Admin2')
+    cy.get('[name="password"]').type('admin1234')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert')
   })
 })
+
+// npx cypress open
